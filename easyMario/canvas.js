38cbbe7fd131836372582137f31eedf2845faf74
Tim Van Herreweghe <tim.van.herreweghe@hotmail.com>
2019-02-18 15:04:24 +0100

    Add files via upload

--- easyMario/canvas.js
+++ easyMario/canvas.js
@@ -1,35 +1,41 @@
 const canvas = document.getElementById('canvas');
 const mario = document.getElementById('mario');
-
-canvas.width = (window.innerWidth / 2);
-canvas.height = (window.innerHeight / 2);
+canvas.width = window.innerWidth / 1.5;
+canvas.height = window.innerHeight / 1.5;
 
 let c = canvas.getContext('2d');
 
 
-
-c.fillStyle = "yellow";
-c.fillRect(20, 450, 20, 20);
-c.stroke();
-
-c.fillStyle = "purple";
-c.fillRect(100, 400, 100, 10);
-c.fillRect(200, 350, 100, 10);
-c.fillRect(300, 300, 100, 10);
-c.fillRect(400, 250, 100, 10);
-c.stroke();
-
-c.beginPath();
-c.arc(530, 200, 30, 0, 2 * Math.PI);
-c.stroke();
-
 window.onload = function(){
-  c.drawImage(mario, 20, 400, 30, 40);  
+  c.fillStyle = "yellow";
+  c.fillRect(20, 450, 20, 20);
+  c.stroke();
+  
+  c.fillStyle = "purple";
+  c.fillRect(100, 400, 100, 10);
+  c.fillRect(200, 350, 100, 10);
+  c.fillRect(300, 300, 100, 10);
+  c.fillRect(400, 250, 100, 10);
+  c.stroke();
+  
+  c.beginPath();
+  c.arc(530, 200, 30, 0, 2 * Math.PI);
+  c.strokeStyle = "blue";
+  c.fillStyle = "yellow";
+  c.stroke();
+  c.fill();
 };
 
-function grow() {
-  return mario.width = 50;
+  let x = 20;
+  let y = 400;
 
-}
+  function animate() {
+    requestAnimationFrame(animate);
 
-mario.addEventListener("click", mario.grow());
+  c.drawImage(mario, x, y, 30, 40);
+  y -= 1;
+};
+
+animate();
+
+

85dc3180ba652ef40a5ce19fa8fb023bc9c3ce3f
Tim Van Herreweghe <tim.van.herreweghe@hotmail.com>
2019-02-22 08:05:40 +0100

    Add files via upload

--- easyMario/canvas.js
+++ easyMario/canvas.js
@@ -1,41 +1,22 @@
 const canvas = document.getElementById('canvas');
 const mario = document.getElementById('mario');
-canvas.width = window.innerWidth / 1.5;
-canvas.height = window.innerHeight / 1.5;
 
 let c = canvas.getContext('2d');
+let x = 50;
+let y = 50;
+let dx = 2;
+let dy = 2;
 
+function drawMario(){
+  c.drawImage(mario, x, y, 50, 30);
+}
 
-window.onload = function(){
-  c.fillStyle = "yellow";
-  c.fillRect(20, 450, 20, 20);
-  c.stroke();
-  
-  c.fillStyle = "purple";
-  c.fillRect(100, 400, 100, 10);
-  c.fillRect(200, 350, 100, 10);
-  c.fillRect(300, 300, 100, 10);
-  c.fillRect(400, 250, 100, 10);
-  c.stroke();
-  
-  c.beginPath();
-  c.arc(530, 200, 30, 0, 2 * Math.PI);
-  c.strokeStyle = "blue";
-  c.fillStyle = "yellow";
-  c.stroke();
-  c.fill();
-};
+function draw() {
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  drawMario();
+  x += dx;
+  y += dy;
+}
 
-  let x = 20;
-  let y = 400;
-
-  function animate() {
-    requestAnimationFrame(animate);
-
-  c.drawImage(mario, x, y, 30, 40);
-  y -= 1;
-};
-
-animate();
-
+setInterval(draw, 100);
 

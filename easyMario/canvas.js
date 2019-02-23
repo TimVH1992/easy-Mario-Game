7b40107fe4432f8100c155aba3b55fbedd18af93
Tim Van Herreweghe <tim.van.herreweghe@hotmail.com>
2019-02-23 11:28:41 +0100

    Add files via upload
    Movement up and left added
    Problem: up - down and right - left ignore eachother; these parameters don't reset yet

--- easyMario/canvas.js
+++ easyMario/canvas.js
@@ -2,21 +2,67 @@ const canvas = document.getElementById('canvas');
 const mario = document.getElementById('mario');
 
 let c = canvas.getContext('2d');
-let x = 50;
-let y = 50;
-let dx = 2;
-let dy = 2;
+let marioX = 50;
+let marioY = 130;
+let marioSpeedX = 2;
+
+// mario control variables
+let marioUp = false;
+let marioRight = false;
+let marioLeft = false;
+let marioDown = false;
+
+
+window.onload = function(){
+  const framesPerSecond = 30;
+  setInterval(function() {
+    c.clearRect(0, 0, innerWidth, innerHeight);
+    drawMario();
+    
+  }, 1000/framesPerSecond);
+
+  // event listeners
+  document.addEventListener('keydown', marioControls, false);
+
+function marioControls(e) {
+  // move up
+  if (e.key == "Up" || e.key == "ArrowUp") {
+    marioUp = true;
+  } 
+    if (marioUp && marioY > 110) {
+        marioY--;
+    }
+  // move left
+  if (e.key == "Left" || e.key == "ArrowLeft") {
+    marioLeft = true;
+  }  
+  if (marioLeft && marioX > 0) {
+    marioX--;
+  }
+  // left and right and up and down ignore eachother
+  // move right
+  /* if (e.key == "Right" || e.key == "ArrowRight") {
+    marioRight = true;
+  }
+  if (marioRight && marioX < canvas.width) {
+    marioX++;
+  }  */
+}
+
+}
+
+
+
+
+/* function moveMario() {
+  marioX = marioX + marioSpeedX;
+   if (marioX - 30 > 250 || marioX < 0) {
+    marioSpeedX = -marioSpeedX;
+  }
+} */
 
 function drawMario(){
-  c.drawImage(mario, x, y, 50, 30);
+  c.drawImage(mario, marioX, marioY, 20, 20);
+  console.log('I am drawing mario');
+  
 }
-
-function draw() {
-  c.clearRect(0, 0, canvas.width, canvas.height);
-  drawMario();
-  x += dx;
-  y += dy;
-}
-
-setInterval(draw, 100);
-
